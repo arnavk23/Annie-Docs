493a83a3bfebe51af63c2636c4d3c61528ec972a
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-07-10 10:50:19 +0530

    Create .lighthouserc.js

---
+++ .lighthouserc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: './build', // or 'dist' depending on your site
+      numberOfRuns: 1,
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
