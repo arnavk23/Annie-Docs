3ad6a024a2eaddd6c075b83bb9e396ff58058229
Akash Bisht <bisht26akash@gmail.com>
2025-07-10 17:51:38 +0530

    added lighthouse in workflow

---
+++ lighthouserc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 3,
+      staticDistDir: null, // we're testing live Netlify preview
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
